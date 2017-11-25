8b131aecb80dc61ee6ae3bd80c8c27484056203d
Marvin Clerge <clergemarvin@gmail.com>
2017-11-25 15:58:09 -0500

    Done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
+require 'pry'
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height
+  attr_accessor :weight, :handed, :complexion, :t_shirt_size
+  attr_accessor :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(arguments)
+    arguments.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+
 end
